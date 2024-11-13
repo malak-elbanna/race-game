826586c276c8c0182991022f6d296451809e40c4
Abdullah <s-abdullah.yasser@zewailcity.edu.eg>
2024-11-13 20:36:13 +0200

    USC complete

---
+++ src/agent/ucs_agent.py
@@ -0,0 +1,92 @@
+import sys
+import os
+import copy
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from src.environment.env import Environment
+from src.environment.terrain import Terrain
+from src.environment.obs_reward import Obstacles
+from src.environment.car import Car
+import heapq
+
+
+def path_cost(path):
+    g_cost = 0
+    for _, cost in path:
+        g_cost += cost
+    return g_cost
+
+def get_successors(environment, path):
+    actions = ["accelerate", "decelerate", "recharge", "move"]
+    state = path[-1][0]
+    position = state[0]  
+    successors = []
+
+    for action in actions:
+        env_copy = copy.deepcopy(environment)
+        env_copy.car.position = position
+        env_copy.car.speed = state[1]
+        env_copy.car.battery = state[2]
+
+        env_copy.step(action)
+        new_state = env_copy.get_state()
+        new_position = new_state[0]
+
+        if new_position > position:
+            segment = env_copy.track.get_segment(new_position)
+            cost = 1
+            new_speed = new_state[1]
+
+            if isinstance(segment, Terrain):
+                cost += max(1, new_speed * segment.get_mult())
+            elif isinstance(segment, Obstacles):
+                cost += max(1, new_speed * 1.5)
+
+            successors.append((new_state, cost))
+    return successors
+
+def ucs(environment, goal):
+    initial_state = environment.get_state()
+    frontier = []
+    heapq.heappush(frontier, (0, [(initial_state, 0)]))  
+    visited = {}
+
+    while frontier:
+        g_cost, path = heapq.heappop(frontier)
+        state = path[-1][0]
+        
+        if state[0] >= goal:
+            return path
+
+        if environment.game_over():
+            continue
+
+        if state in visited and visited[state] <= g_cost:
+            continue
+
+        visited[state] = g_cost
+
+        successors = get_successors(environment, path)
+        for new_state, cost in successors:
+            new_path = path + [(new_state, cost)]
+            new_g_cost = path_cost(new_path)
+            if new_state not in visited or visited[new_state] > new_g_cost:
+                heapq.heappush(frontier, (new_g_cost, new_path))
+
+    return None  
+
+def main():
+    env = Environment(track_length=100)
+    print(env)  
+    solution = ucs(env, env.track.length - 1)
+
+    if solution:
+        print("Solution path:", solution)
+        total_cost = 0
+        for _, cost in solution:
+            total_cost += cost
+
+        print("Total cost: ", total_cost)
+    else:
+        print("No solution")
+
+main()
