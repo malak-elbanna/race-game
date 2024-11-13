c69d9e6ec72ef53756ac73f252900e12eeaeb5f0
Kahla <s-ahmed.kahla@zewailcity.edu.eg>
2024-11-13 21:06:51 +0200

    dfs and ids

---
+++ src/agent/dfs_agent.py
@@ -0,0 +1,70 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from src.environment.env import Environment
+from src.environment.terrain import Terrain
+from src.environment.obs_reward import Obstacles
+from src.environment.car import Car
+from collections import deque
+import copy
+
+def get_successors(environment, path):
+    actions = ["accelerate", "decelerate", "recharge", "move"]
+    state = path[-1][0]
+    position = state[0]
+    successors = []
+
+    for action in actions:
+        env_copy = copy.deepcopy(environment)
+        env_copy.car.position = position
+        env_copy.car.speed = state[1]
+        env_copy.car.battery = state[2]
+
+        env_copy.step(action)
+        new_state = env_copy.get_state()
+        new_position = new_state[0]
+
+        if new_position > position:
+            successors.append((new_state, 1))
+    return successors
+
+def dfs(environment, goal):
+    initial_state = environment.get_state()
+    frontier = deque([[(initial_state, 0)]])  
+    visited = set()
+
+    while frontier:
+        path = frontier.pop() 
+        state = path[-1][0]
+
+        if state[0] >= goal:
+            return path
+
+        if environment.game_over():
+            continue
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+        successors = get_successors(environment, path)
+
+        for new_state, i in successors:
+            if new_state not in visited:
+                new_path = path + [(new_state, 1)]
+                frontier.append(new_path)
+
+    return None
+
+def main():
+    env = Environment(track_length=40)
+    solution = dfs(env, env.track.length - 1)
+
+    if solution:
+        print("Solution path:", solution)
+        total_steps = len(solution) - 1
+        print("Total steps:", total_steps)
+    else:
+        print("No solution")
+
+main()
---
+++ src/agent/ids_agent.py
@@ -0,0 +1,81 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from src.environment.env import Environment
+from src.environment.terrain import Terrain
+from src.environment.obs_reward import Obstacles
+from src.environment.car import Car
+from collections import deque
+
+def get_successors(environment, path):
+    actions = ["accelerate", "decelerate", "recharge", "move"]
+    state = path[-1][0]
+    position = state[0]
+    successors = []
+    seen_states = set()
+
+    for action in actions:
+        environment.car.position = position
+        environment.car.speed = state[1]
+        environment.car.battery = state[2]
+
+        environment.step(action)
+        new_state = environment.get_state()
+        new_position = new_state[0]
+
+        if new_position > position and new_state not in seen_states:
+            successors.append((new_state, 1))
+            seen_states.add(new_state) 
+    return successors
+
+def dfs_limited(environment, goal, limit):
+    initial_state = environment.get_state()
+    frontier = deque([[(initial_state, 0)]]) 
+    visited = set()  
+
+    while frontier:
+        path = frontier.pop() 
+        state = path[-1][0]
+
+        if state[0] >= goal:
+            return path
+
+        if len(path) - 1 >= limit:
+            continue
+
+        if environment.game_over():
+            continue
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+        successors = get_successors(environment, path)
+
+        for new_state, _ in successors:
+            if new_state not in visited:
+                new_path = path + [(new_state, 1)]
+                frontier.append(new_path)
+
+    return None
+
+def ids(environment, goal):
+    depth = 0
+    while True:
+        result = dfs_limited(environment, goal, depth)
+        if result:
+            return result
+        depth += 1
+
+def main():
+    env = Environment(track_length=40)
+    solution = ids(env, env.track.length - 1) 
+
+    if solution:
+        print("Solution path:", solution)
+        total_steps = len(solution) - 1
+        print("Total steps:", total_steps)
+    else:
+        print("No solution")
+
+main()
